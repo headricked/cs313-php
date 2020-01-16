9ec9bc647001d453a22101fe56cf431de9c6f285
David Headrick <davidheadrick1@gmail.com>
2020-01-15 23:25:18 -0500

    updated css file

--- web/index.js
+++ web/index.js
@@ -2,9 +2,9 @@
 function hello() {
 	var x = document.getElementById("hello");
 
-  if (x.style.display === "block") {
-  	x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
   	x.style.display = "block";
+  } else {
+  	x.style.display = "none";
   }
 }
