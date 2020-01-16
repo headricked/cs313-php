177060723c4f8e587ad1d72b8aaafff9415441b5
David Headrick <davidheadrick1@gmail.com>
2020-01-15 23:39:18 -0500

    updated css file

--- web/index.js
+++ web/index.js
@@ -2,11 +2,19 @@
 function hello() {
 	var x = document.getElementById("hello");
 
-  if (x.style.display === "hidden") {
-//   	x.style.display = "block";
-//   	x.style.background-color = "none";
+//   if (x.style.display === "none") {
+// //   	x.style.display = "block";
+// //   	x.style.background-color = "none";
+// 		x.style.background = url('HeadrickDavid.jpg')
+//   } else {
+//   	x.style.display = "none";
+//   }
+
+  if (x.style.visibility === "visible") {
 		x.style.background = url('HeadrickDavid.jpg');
+		x.style.visibility = "hidden";
   } else {
-  	x.style.display = "hidden";
+  	x.style.display = "visible";
   }
+
 }
