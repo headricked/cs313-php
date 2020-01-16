c0b53dfb19276601323ee14881d6a5559f099b0e
David Headrick <davidheadrick1@gmail.com>
2020-01-15 23:30:47 -0500

    updated css file

--- web/index.js
+++ web/index.js
@@ -2,11 +2,11 @@
 function hello() {
 	var x = document.getElementById("hello");
 
-  if (x.style.display === "none") {
+  if (x.style.display === "hidden") {
 //   	x.style.display = "block";
 //   	x.style.background-color = "none";
 		x.style.background = url('HeadrickDavid.jpg');
   } else {
-  	x.style.display = "none";
+  	x.style.display = "hidden";
   }
 }
