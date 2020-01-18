6c9e978be5c0e9e2b3d437466beb62266a680239
David Headrick <davidheadrick1@gmail.com>
2020-01-18 15:24:37 -0500

    updated file

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,12 @@
-<?php
-  echo "<p>Copyright &copy; " . date("Y") . " David Headrick</p>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>footer.php</title>
+  </head>
+  <body>
+    <?php
+      echo "<p>Copyright &copy; " . date("Y") . " David Headrick</p>";
+    ?>
+  </body>
+</html>
