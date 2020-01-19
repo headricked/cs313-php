1c0d303525d38a5774165be73856de29518f7f30
David Headrick <davidheadrick1@gmail.com>
2020-01-18 21:09:59 -0500

    updated file

--- web/header.php
+++ web/header.php
@@ -18,11 +18,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>                        
                 </button>
-                <!-- <a class="navbar-brand" href="index.php">David Headrick | Portfolio</a> -->
+                <a class="navbar-brand" href="index.php">Portfolio</a>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.php">Portfolio</a></li>
+                        <!-- <li class="active"><a href="index.php">Portfolio</a></li> -->
                         <li><a href="week02.html">Week02</a></li>
                         <li><a href="#">Week03</a></li>
                         <li><a href="#">Week04</a></li>
