6d79594b881b6b1b362d3510897d108874b7eb8f
David Headrick <davidheadrick1@gmail.com>
2020-01-25 14:14:06 -0500

    added files

---
+++ web/week03/shoppingCart/add-to-cart.php
@@ -0,0 +1,15 @@
+<?php
+    // Start session
+    session_start();
+
+    if (empty($_SESSION['cart'])) {
+        $_SESSION['cart'] = array();
+    }
+
+    array_push($_SESSION['cart'], $_GET['id']);
+
+?>
+
+<p>Product was successfully added to your shopping cart.
+    <a href="shopping-cart.php">View shopping cart</a>
+</p>
