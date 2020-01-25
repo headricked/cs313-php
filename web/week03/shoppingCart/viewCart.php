8128c75fac4c17768599d871e752846ad921a066
David Headrick <davidheadrick1@gmail.com>
2020-01-25 14:17:34 -0500

    updated file

--- web/week03/shoppingCart/viewCart.php
+++ web/week03/shoppingCart/viewCart.php
@@ -14,8 +14,7 @@
 <body>
     <?php
         // Echo session variables that were set on previous page
-        echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-        echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+        echo "Your cart: " . $_SESSION['cart'] . ".<br>";
     ?>    
 </body>
 </html>
