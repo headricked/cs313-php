15fc8bdf6cdcd133f50a3439f5dc2b8624e238f3
David Headrick <davidheadrick1@gmail.com>
2020-01-25 17:27:12 -0500

    updated file

--- web/week03/shoppingCart/confirmation.php
+++ web/week03/shoppingCart/confirmation.php
@@ -1,3 +1,43 @@
 <?php
-
+    $name = htmlspecialchars($_POST['name']);
+    $address = htmlspecialchars($_POST['address']);
+    $city = htmlspecialchars($_POST['city']);
+    $state = htmlspecialchars($_POST['state']);
+    $zip = $_POST['zip'];
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+	    Name: <?php echo $name; ?>
+	</div>
+    
+    <div>
+        Address: <?php echo $address; ?>
+        
+	</div>
+    
+    <div>
+	    City: <?php echo $city; ?>
+	</div>
+    
+    <div>
+	    State: <?php echo $state; ?>
+	</div>
+    
+    <div>
+	    ZIP: <?php echo $zip; ?>
+	</div>
+    
+</body>
+</html>
