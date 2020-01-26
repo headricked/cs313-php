a894e0713cd6f3a5735636d77b48e0beafe3c0df
David Headrick <davidheadrick1@gmail.com>
2020-01-25 23:18:10 -0500

    updated file

--- web/week03/shoppingCart/script.js
+++ web/week03/shoppingCart/script.js
@@ -1,16 +1,20 @@
 document.getElementById('samsung').onclick = function() {
-    $.post("2-cart-add.php", { name: "samsung", price: 700 })
+    $.post("2-cart-add.php", { name: "samsung"})
 };
 
-document.getElementById('samsung').onclick = function() {
-    $.post("2-cart-add.php", { name: "apple", price: 600 })
-};
+// document.getElementById('samsung').onclick = function() {
+//     $.post("2-cart-add.php", { name: "samsung", price: 700 })
+// };
 
-document.getElementById('samsung').onclick = function() {
-    $.post("2-cart-add.php", { name: "google", price: 500 })
-};
+// document.getElementById('samsung').onclick = function() {
+//     $.post("2-cart-add.php", { name: "apple", price: 600 })
+// };
 
-document.getElementById('samsung').onclick = function() {
-    $.post("2-cart-add.php", { name: "motorola", price: 400 })
-};
+// document.getElementById('samsung').onclick = function() {
+//     $.post("2-cart-add.php", { name: "google", price: 500 })
+// };
+
+// document.getElementById('samsung').onclick = function() {
+//     $.post("2-cart-add.php", { name: "motorola", price: 400 })
+// };
 
