7ffdf8c0aee51d83b456ffa3ad09e2c1aa9e2e5b
David Headrick <davidheadrick1@gmail.com>
2020-01-25 20:09:02 -0500

    updated file

--- web/week03/shoppingCart/2-cart-add.php
+++ web/week03/shoppingCart/2-cart-add.php
@@ -13,7 +13,7 @@
 <body>
 
 <!-- <form method=post action=''> -->
-<form method="post" action="">
+<form method="post" action="2-cart-display.php">
     Enter a product name <input type="text" name="product">
     <input type="submit" value="Add to Cart">
 </form>
