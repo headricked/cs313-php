8542da260d5887246a4b0efaa522cf82100beaae
David Headrick <davidheadrick1@gmail.com>
2020-01-25 19:25:30 -0500

    updated file

--- web/week03/shoppingCart/2-cart.php
+++ web/week03/shoppingCart/2-cart.php
@@ -14,10 +14,15 @@
     array_push($_SESSION['cart'],'apple','mango','banana','orange'); // Items added to cart
 
     echo    "Number of Items in the cart = " .
-            sizeof($_SESSION['cart']) .
-            // " <a href=2-cart-remove-all.php>Remove all</a><br>";
-            " <a href=2-cart-remove.php>Remove</a><br>";
+            sizeof($_SESSION['cart']);
+    // " <a href=2-cart-remove-all.php>Remove all</a><br>";
+    // " <a href=2-cart-remove.php>Remove</a>";
 ?>
+
+<div><a href=2-cart-add.php>Add</a></div>
+<div><a href=2-cart-display.php>Display</a></div>
+<div><a href=2-cart-remove.php>Remove</a></div>
+
 
 </body>
 </html>
