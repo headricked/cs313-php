2c0fab9dcf97938edca26247cdc2cbd5fe0a85b9
David Headrick <davidheadrick1@gmail.com>
2020-01-25 22:56:11 -0500

    added file

---
+++ web/week03/shoppingCart/script.js
@@ -0,0 +1,16 @@
+document.getElementById('samsung').click(function() {
+    $.post("2-cart-add.php", { name: "samsung", price: 700 })
+});
+
+document.getElementById('samsung').click(function() {
+    $.post("2-cart-add.php", { name: "apple", price: 600 })
+});
+
+document.getElementById('samsung').click(function() {
+    $.post("2-cart-add.php", { name: "google", price: 500 })
+});
+
+document.getElementById('samsung').click(function() {
+    $.post("2-cart-add.php", { name: "motorola", price: 400 })
+});
+
