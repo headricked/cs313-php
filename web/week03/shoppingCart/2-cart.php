9be894fbc791fd400c4ec79e01fdfc7c7f5d8ada
David Headrick <davidheadrick1@gmail.com>
2020-01-26 00:45:58 -0500

    updated file

--- web/week03/shoppingCart/2-cart.php
+++ web/week03/shoppingCart/2-cart.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    // if (!isset($_SESSION["cart"]))
-    // {
-    //     $_SESSION["cart"] = array();
-    // }
-
-    if(!isset($_POST['cart'])){ 
-        $_SESSION['cart'][] = $_POST['cart'];
+    if (!isset($_SESSION["cart"]))
+    {
+        $_SESSION["cart"][] = array();
     }
+
+    // if(!isset($_POST['cart'])){ 
+    //     $_SESSION['cart'][] = $_POST['cart'];
+    // }
 ?>
 
 <!DOCTYPE html>
