489f88944167dd9d939ed0e56eebe9eb33103f3f
David Headrick <davidheadrick1@gmail.com>
2020-01-25 23:33:08 -0500

    updated file

--- web/week03/shoppingCart/script.js
+++ web/week03/shoppingCart/script.js
@@ -1,6 +1,34 @@
+// document.getElementById('samsung').onclick = function() {
+//     $.post("2-cart-add.php", { name: "samsung"})
+// };
+
 document.getElementById('samsung').onclick = function() {
-    $.post("2-cart-add.php", { name: "samsung"})
+    // Vanilla
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function (data) {
+    // code
+    };
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.open('POST', '2-cart-add.php');
+    httpRequest.send('name=' + encodeURIComponent(name));
 };
+
+
+
+// // jQuery
+// $.post('//example.com', { username: username }, function (data) {
+//   // code
+// })
+
+// // Vanilla
+// var httpRequest = new XMLHttpRequest();
+// httpRequest.onreadystatechange = function (data) {
+//   // code
+// };
+// httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// httpRequest.open('POST', '2-cart-add.php');
+// httpRequest.send('name=' + encodeURIComponent(name));
+
 
 // document.getElementById('samsung').onclick = function() {
 //     $.post("2-cart-add.php", { name: "samsung", price: 700 })
