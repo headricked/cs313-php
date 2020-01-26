a7303f0ef3ad99024aad6dbe374f40db143662d9
David Headrick <davidheadrick1@gmail.com>
2020-01-25 19:46:23 -0500

    updated file

--- web/week03/shoppingCart/2-cart-remove-all.php
+++ web/week03/shoppingCart/2-cart-remove-all.php
@@ -2,13 +2,14 @@
     session_start();
 ?>
 
-<!doctype html public "-//w3c//dtd html 3.2//en">
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cart Remove All</title>
 </head>
-
 <body>
 
 <?php
