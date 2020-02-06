9385c49c0c1314d0c5b13f0e89bf68f2471c3062
David Headrick <davidheadrick1@gmail.com>
2020-02-05 22:02:30 -0500

    updated files

--- web/week05/week05_team.sql
+++ web/week05/week05_team.sql
@@ -4,7 +4,7 @@ CREATE TABLE scriptures (
     chapter SMALLINT NOT NULL,
     verse SMALLINT NOT NULL,
     content TEXT NOT NULL
-)
+);
 
 INSERT INTO scriptures (id, book, chapter, verse, content)
     VALUES (DEFAULT, 'John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.');
