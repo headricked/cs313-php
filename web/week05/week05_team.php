b298f8e1073bb1e6d979c91d91d5ab2463aafb45
David Headrick <davidheadrick1@gmail.com>
2020-02-05 22:13:55 -0500

    updated files

--- web/week05/week05_team.php
+++ web/week05/week05_team.php
@@ -1,48 +1,49 @@
 <?php
-    try
-    {
-        $dbUrl = getenv('DATABASE_URL');
-        $dbOpts = parse_url($dbUrl);
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    
-    catch (PDOException $ex)
-    {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-    }
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Scripture Reference</title>
+<title>Scripture Resourses</title>
 </head>
 <body>
 
 <h1>Scripture Resources</h1>
 
+
 <?php
+foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures') as $row)
+{
+  echo "<div><b>" . $row["book"] . ":" . $row["chapter"] . " " . $row["verse"] . "- " . $row["content"] . "</b></div>";   
+  echo '<br/>';
+}
 
-    echo "Hello World!";
-
-    foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures') as $row)
-    {
-        echo
-            '<div><strong>' .
-            $row['book'] .
-            '</strong></div>';
-    }
 ?>
+
+
 
 </body>
 </html>
