eceb1b422249efa56ae31e51f4dfbcafda9d6735
David Headrick <davidheadrick1@gmail.com>
2020-02-05 23:00:15 -0500

    updated files

--- web/week05/week05_team.php
+++ web/week05/week05_team.php
@@ -1,46 +1,51 @@
 <?php
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
 
-  $dbOpts = parse_url($dbUrl);
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 ?>
-
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Scripture Resourses</title>
-</head>
-<body>
-
-<h1>Scripture Resources</h1>
-
-
-<?php
-foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures') as $row)
-{
-  echo "<div><b>" . $row["book"] . ":" . $row["chapter"] . " " . $row["verse"] . "- " . $row["content"] . "</b></div>";   
-  echo '<br/>';
-}
-
+  <head>
+    <title>Scripture Resourses</title>
+  </head>
+  <body>
+  
+  <h1>Scripture Resources</h1>
+  
+  <?php
+    foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures') as $row)
+    {
+      echo
+        "<div><strong>" .
+        $row["book"] .
+        " " .
+        $row["chapter"] .
+        ":" .
+        $row["verse"] .
+        "</b> - \"" .
+        $row["content"] .
+        "\"" . "</div>";
+      }
 ?>
 
 
