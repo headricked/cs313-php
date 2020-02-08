9a23740c5e61d9c9f712ab98231b0e5bae98ce02
David Headrick <davidheadrick1@gmail.com>
2020-02-07 23:54:20 -0500

    updated files

--- web/milestone/details.php
+++ web/milestone/details.php
@@ -15,13 +15,14 @@
             SELECT * FROM milestone
             INNER JOIN person
             ON milestone.person_id = person.person_id
-            WHERE person.person_id = ':person_id')
+            WHERE person.person_id = :person_id)
        
        SELECT milestone_name, milestone_date, DATE_PART('year', milestone_date) - DATE_PART('year', birthdate)
             AS person_age, milestone_location, milestone_notes
             FROM theEvent;
     ");
-    $statement_person->bindValue(':person_id', $person_id, PDO::PARAM_INT);
+    // $statement_person->bindValue(':person_id', $person_id, PDO::PARAM_INT);
+    $statement_milestone->bindValue(':person_id', $person_id, PDO::PARAM_INT);
     $statement_milestone->execute();
 
     // query the database for person data
