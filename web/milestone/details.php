fd6005e34dd9e90369d1928df01250e3618e7633
David Headrick <davidheadrick1@gmail.com>
2020-02-09 01:03:30 -0500

    updated files

--- web/milestone/details.php
+++ web/milestone/details.php
@@ -243,7 +243,7 @@
             <textarea name="milestone_notes" placeholder="notes" rows="10"></textarea>
           <hr/>
 
-          <input type="hidden" name="person_id" value="21">
+          <input type="hidden" name="person_id" value="<?php htmlspecialchars($_GET['person_id']) ?>">
 
             <input type="submit" value="ADD">
         </form>
