9c6b3509e8c9daa81ae1748df67da545429e894c
David Headrick <davidheadrick1@gmail.com>
2020-02-09 01:54:03 -0500

    updated files

--- web/milestone/details.php
+++ web/milestone/details.php
@@ -46,7 +46,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Scripture Details</title>
+    <title>Milestones for <?php echo $full_name ?></title>
 </head>
 <body>
     <h1><?php echo $full_name ?></h1>
