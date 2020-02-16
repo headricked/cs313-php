c0c0c661afbdba6ddae15368e8f05dfdc9832ff2
David Headrick <davidheadrick1@gmail.com>
2020-02-16 01:14:12 -0500

    updated files

--- web/milestone/details.php
+++ web/milestone/details.php
@@ -49,6 +49,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Milestones for <?php echo $full_name ?></title>
     <link rel="stylesheet" href="milestone.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>  </head>
 </head>
 <body>
     <h1><?php echo $full_name ?></h1>
