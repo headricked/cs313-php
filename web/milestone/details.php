737f4eebf90baa8bff50881b6bd0ab63f5122d67
David Headrick <davidheadrick1@gmail.com>
2020-02-15 23:25:39 -0500

    updated files

--- web/milestone/details.php
+++ web/milestone/details.php
@@ -47,6 +47,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Milestones for <?php echo $full_name ?></title>
+    <link rel="stylesheet" href="milestone.css">
 </head>
 <body>
     <h1><?php echo $full_name ?></h1>
