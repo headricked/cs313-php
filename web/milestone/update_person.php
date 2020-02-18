f374084bf46cfbfd91c603f382983a8962462b59
David Headrick <davidheadrick1@gmail.com>
2020-02-18 00:16:00 -0500

    updated files

--- web/milestone/update_person.php
+++ web/milestone/update_person.php
@@ -34,11 +34,11 @@
     // update to person in person table
     $stmt_person = $db->prepare("
                                 UPDATE person
-                                SET first_name  = '$new_first_name',
-                                    middle_name = '$new_middle_name',
-                                    last_name   = '$new_last_name',
-                                    is_male     = '$new_is_male'
-                                WHERE person_id = '$person_id';
+                                SET first_name  = ':new_first_name',
+                                    middle_name = ':new_middle_name',
+                                    last_name   = ':new_last_name',
+                                    is_male     = ':new_is_male'
+                                WHERE person_id = ':person_id';
                                 ");
     $stmt_person->bindValue(':new_first_name',  $new_first_name,  PDO::PARAM_STR);
     $stmt_person->bindValue(':new_middle_name', $new_middle_name, PDO::PARAM_STR);
