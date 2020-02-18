0ff353b02101a8c7a684442d599c0487d3b8ca1a
David Headrick <davidheadrick1@gmail.com>
2020-02-17 20:49:57 -0500

    updated files

--- web/milestone/index.php
+++ web/milestone/index.php
@@ -12,12 +12,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>  </head>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </head>
 
   <body>
 
   <div>
-    <h1>Milestone Tracker</h1>
+    <p>Milestone Tracker</p>
     <!-- Button to Open the Modal -->
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Add a person</button>
   </div>
