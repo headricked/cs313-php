78a4db21b172688b8a903dddd4e473cd28bb3e00
David Headrick <davidheadrick1@gmail.com>
2020-02-19 22:24:13 -0500

    updated files

---
+++ web/week07/signup.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<?php
+
+
+
+    $new_page = "signin.php";
+    header("Location: $new_page");
+    die();
+
+?>
