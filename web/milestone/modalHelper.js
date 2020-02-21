13fbf6688ce3a4e0778a94c0d9cfe107f8f23d17
David Headrick <davidheadrick1@gmail.com>
2020-02-20 23:44:21 -0500

    updated files

--- web/milestone/modalHelper.js
+++ web/milestone/modalHelper.js
@@ -19,7 +19,7 @@ $('#modal_updateMilestone').on('show.bs.modal', function (event)
         // modal.find('#mLocation').val(m_location);
         // modal.find('#mNotes').val(m_notes);
 
-        alert('button: ' + button.val);
+        alert('event.relatedTarget: ' + event.relatedTarget);
         // alert('milestone name: ' + m_name);
 
     }
