6f908d731770efcf292c21a552dd55daaac05cd7
David Headrick <davidheadrick1@gmail.com>
2020-02-22 15:25:09 -0500

    updated file

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,5 +1,3 @@
--- CREATE DATABASE milestone;
-
 CREATE TABLE person (
     person_id SERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
@@ -8,15 +6,6 @@ CREATE TABLE person (
     birthdate TIMESTAMP NOT NULL,
     is_male BOOLEAN NOT NULL
 );
-
--- CREATE TABLE milestone (
---     milestone_id SERIAL NOT NULL PRIMARY KEY,
---     milestone_name VARCHAR(100) NOT NULL,
---     milestone_date TIMESTAMP NOT NULL,
---     milestone_location VARCHAR(100) NOT NULL,
---     milestone_notes VARCHAR(500),
---   	person_id INT NOT NULL REFERENCES person(person_id)
--- );
 
 CREATE TABLE milestone (
     milestone_id SERIAL NOT NULL PRIMARY KEY,
@@ -27,67 +16,8 @@ CREATE TABLE milestone (
   	person_id INT NOT NULL REFERENCES person(person_id) ON DELETE CASCADE
 );
 
--- test data below
--- EMMETT DOC BROWN
-INSERT INTO person (person_id, first_name, middle_name, last_name, birthdate, is_male)
-    VALUES (DEFAULT,'Emmett','Doc','Brown','1914-11-05 12:34:56',true);
 
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Birth','1914-11-05 12:34:56','Hill Valley, California','Born with white hair', 1);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'First words','1915-12-28 12:34:56','Hill Valley, California','"flux capacitor"', 1);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Concussion','1955-11-05 12:34:56','Hill Valley, California','hit head on toilet; a red-letter date', 1);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Time Travel Success','1985-10-26 01:20:00','Twin Pines Mall, Hill Valley, California','Docs dog, Einstein, becomes the worlds first time traveler.', 1);
-
--- MARTIN SEAMUS MCFLY
-INSERT INTO person (person_id, first_name, middle_name, last_name, birthdate, is_male)
-    VALUES (DEFAULT,'Martin','Seamus','McFly','1968-06-20 12:34:56',true);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Birth','1968-06-20 12:34:56','Hill Valley, California','Wrapped in a life preserver because there were no blankets to keep him warm.', 2);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Travel Back in Time','1985-10-26 01:20:00','Twin Pines Mall, Hill Valley, California','Eludes Lybians by travelling back in time.', 2);
-
--- GEORGE DOUGLAS MCFLY
-INSERT INTO person (person_id, first_name, middle_name, last_name, birthdate, is_male)
-    VALUES (DEFAULT,'George','Douglas','McFly','1938-04-01 12:34:56',true);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Birth','1938-04-01 12:34:56','Hill Valley, California','Born with dark brown hair.', 3);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Meets future son, Marty','1955-11-05 12:34:56','Lous Cafe, Hill Valley, California','Bullied by Biff.', 3);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Visited by Darth Vader','1955-11-08 12:23:00','Home, Hill Valley, California','Ordered to ask Lorraine for a date or Vader would melt his brain.', 3);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Won love of Lorraine, future wife','1955-11-12 21:05:00','Hill Valley High School, Hill Valley, California','Punched Biff to stand up for Lorraine at the Enchantment Under the Sea Dance.', 3);
-
--- LORRAINE BAINES
-INSERT INTO person (person_id, first_name, middle_name, last_name, birthdate, is_male)
-    VALUES (DEFAULT,'Lorraine','Baines','McFly','1938-01-01 00:00:00',false);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Birth','1938-01-01 00:00:00','Hill Valley, California','Born with dark brown hair.', 4);
-
-INSERT INTO milestone (milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id)
-	VALUES (DEFAULT,'Fell in love with George','1955-11-12 21:05:00','Hill Valley High School, Hill Valley, California','Enchantment Under the Sea Dance.', 4);
-
-
-
--- WITH event AS (
---   SELECT * FROM milestone 
--- INNER JOIN person ON milestone.person_id = person.person_id)
-
--- SELECT *, DATE_PART('year', milestone_date) - DATE_PART('year', birthdate) AS person_age FROM event;
-
+-- QUERY TO FOR INDIVIDUAL MILESTONE
 WITH theEvent AS (
   SELECT * FROM milestone
   INNER JOIN person
@@ -97,7 +27,7 @@ WITH theEvent AS (
   AS person_age, milestone_location, milestone_notes
   FROM theEvent;
 
--- milstones for emmett doc brown
+-- milstones for user with person_id = 1
 WITH theEvent AS (
   SELECT * FROM milestone
   INNER JOIN person
@@ -119,6 +49,7 @@ WITH theEvent AS (
   AS person_age, milestone_location, milestone_notes
   FROM theEvent;
 
+-- COMMAND TO UPDATE RECORD FOR person_id = 10
 UPDATE person
   SET first_name  = 'Mary',
       middle_name = 'Tyler',
@@ -126,6 +57,7 @@ UPDATE person
       is_male     = 'false'
   WHERE person_id = 10;
 
+-- QUERY USED TO POPULATE 'UPDATE MILESTONE' MODAL DATA FOR milestone_id = 43
 SELECT milestone_id, milestone_name, milestone_date, milestone_location, milestone_notes, person_id
   FROM milestone
   WHERE person_id = 3
