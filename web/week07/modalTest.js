befa5a7791401df30f5b2f6db3ca334c34235f4a
David Headrick <davidheadrick1@gmail.com>
2020-02-22 00:18:06 -0500

    debugging

--- web/week07/modalTest.js
+++ web/week07/modalTest.js
@@ -3,13 +3,19 @@ $('#exampleModal').on('show.bs.modal', function (event)
         var button = $(event.relatedTarget); // Button that triggered the modal
         var one = button.data('whatever1'); // Extract info from data-* attributes
         var two = button.data('whatever2'); // Extract info from data-* attributes
+
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+
         var modal = $(this);
-        modal.find('.modal-title').text('New message to ' + one);
+
+        // modal.find('.modal-title').text('New message to ' + one);
         // modal.find('.modal-body input').val(recipient);
-        modal.find('#recipient-name').val(one);
+
+        modal.find('#mName').val(one);
+
         // modal.find('.modal-body textarea').val(recipient + "Doody");
+
         modal.find('#message-text').val(two);
         
     }
